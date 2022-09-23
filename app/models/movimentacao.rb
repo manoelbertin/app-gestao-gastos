f24b16b636188f55c5842f8b9cc8770b1733c59a
manoel bertin <manoeltirj@gmail.com>
2022-09-23 08:41:12 -0300

    refatorar

--- app/models/movimentacao.rb
+++ app/models/movimentacao.rb
@@ -1,3 +1,7 @@
 class Movimentacao < ApplicationRecord
   enum :tipo, { saida: 'saida', entrada: 'entrada'}
+
+  entradas = Movimentacao.entrada.sum(:valor)
+    saidas = Movimentacao.saida.sum(:valor)
+
 end
