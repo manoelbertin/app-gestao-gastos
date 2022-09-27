3745af1f3590f9427c25889bf8ed351c89295e09
manoel bertin <manoeltirj@gmail.com>
2022-09-26 22:03:57 -0300

    redirect-to movimentacoes_url

--- app/controllers/movimentacoes_controller.rb
+++ app/controllers/movimentacoes_controller.rb
@@ -7,49 +7,30 @@ class MovimentacoesController < ApplicationController
     @saldo = Movimentacao.saldo_atual
   end
 
-  def show
-  end
-  # GET /movimentacoes/new
+   # GET /movimentacoes/new
   def new
     @movimentacao = Movimentacao.new
   end
 
-  def edit
-  end
-  # POST /movimentacoes or /movimentacoes.json
+    # POST /movimentacoes or /movimentacoes.json
   def create
     @movimentacao = Movimentacao.new(movimentacao_params)
 
     respond_to do |format|
       if @movimentacao.save
-        format.html { redirect_to movimentacao_url(@movimentacao), notice: "Movimentacao was successfully created." }
-        format.json { render :show, status: :created, location: @movimentacao }
+        format.html { redirect_to movimentacoes_url, notice: "Movimentação foi criada com sucesso." }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @movimentacao.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /movimentacoes/1 or /movimentacoes/1.json
-  def update
-    respond_to do |format|
-      if @movimentacao.update(movimentacao_params)
-        format.html { redirect_to movimentacao_url(@movimentacao), notice: "Movimentacao was successfully updated." }
-        format.json { render :show, status: :ok, location: @movimentacao }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @movimentacao.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /movimentacoes/1 or /movimentacoes/1.json
+   # DELETE /movimentacoes/1 or /movimentacoes/1.json
   def destroy
     @movimentacao.destroy
 
     respond_to do |format|
-      format.html { redirect_to movimentacoes_url, notice: "Movimentacao was successfully destroyed." }
+      format.html { redirect_to movimentacoes_url, notice: "Movimentação foi apagada com sucesso." }
       format.json { head :no_content }
     end
   end
