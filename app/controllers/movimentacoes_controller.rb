8fc012f98e63550736153ffe70fd63ea95056000
manoel bertin <manoeltirj@gmail.com>
2022-09-28 17:14:58 -0300

    ajustei o devise para logout com o get

--- app/controllers/movimentacoes_controller.rb
+++ app/controllers/movimentacoes_controller.rb
@@ -4,18 +4,18 @@ class MovimentacoesController < ApplicationController
 
   # GET /movimentacoes or /movimentacoes.json
   def index
-    @movimentacoes = Movimentacao.order(data: :desc, created_at: :desc)
-    @saldo = Movimentacao.saldo_atual
+    @movimentacoes = collection.order(data: :desc, created_at: :desc)
+    @saldo = collection.saldo_atual
   end
 
    # GET /movimentacoes/new
   def new
-    @movimentacao = Movimentacao.new
+    @movimentacao = collection.new
   end
 
     # POST /movimentacoes or /movimentacoes.json
   def create
-    @movimentacao = Movimentacao.new(movimentacao_params)
+    @movimentacao = collection.new(movimentacao_params)
 
     respond_to do |format|
       if @movimentacao.save
@@ -37,9 +37,13 @@ class MovimentacoesController < ApplicationController
   end
 
   private
+
+    def collection 
+      current_user.movimentacoes
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_movimentacao
-      @movimentacao = Movimentacao.find(params[:id])
+      @movimentacao = collection.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
