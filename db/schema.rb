342f6556d58025d8b7ae23b6245b30cf7b21c2d8
manoel bertin <manoeltirj@gmail.com>
2022-09-27 23:22:26 -0300

    antes de tudo autenticar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_210723) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_021928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_210723) do
     t.string "tipo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_movimentacoes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_210723) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "movimentacoes", "users"
 end
